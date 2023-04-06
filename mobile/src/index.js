7c87db8e6c0a9ad58404a5d15b654d34f9b7902a
Mauro Jr <mauro.jr@icloud.com>
2023-04-06 14:18:56 -0400

    Adicionei codigos iniciais do mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,38 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './login';
+import SignupScreen from './signup';
+import FeedScreen from './feed';
+import CadastroScreen from './cadastro';
+import DetalhesScreen from './detalhes';
+import SearchScreen from './procurar';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs({ navigation }) {
+	return (
+		<Tab.Navigator initialRouteName='Feed'>
+			<Tab.Screen name='Procurar' component={SearchScreen} />
+			<Tab.Screen name='Feed' component={FeedScreen} />
+			<Tab.Screen name='Cadastro' component={RegisterScreen} />
+		</Tab.Navigator>
+	);
+}
+
+export default function MeuApp() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName='Signup'>
+				<Stack.Screen name='Login' component={LoginScreen} />
+				<Stack.Screen name='Signup' component={SignupScreen} />
+				<Stack.Screen name='Home' component={Tabs} />
+				<Stack.Screen name='Detalhes' component={DetalhesScreen} />
+				<Stack.Screen name='Procurar' component={SearchScreen} />
+				<Stack.Screen name='Cadastro' component={CadastroScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
---
+++ mobile/src/login/estilo.js
@@ -0,0 +1,26 @@
+//importar stylesheet da biblioteca RN
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'yellow',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	logoContainer: {
+		flex: 1,
+		backgroundColor: 'green',
+		width: '100%',
+	},
+	inputContainer: {
+		flex: 2,
+		backgroundColor: 'orange',
+		width: '100%',
+	},
+	signupContainer: {
+		flex: 1,
+		backgroundColor: 'red',
+		width: '100%',
+	},
+});
